2c77638f7269a7b6bcba0a8aa433f8ce5840b877
Luiz <labm1997@gmail.com>
2019-06-05 07:59:02 -0300

    Refactoring: Added comments New Feature: Added toQString method that converts parsed HTTP to a request

--- include/httpparser.h
+++ include/httpparser.h
@@ -8,33 +8,55 @@
 
 #include "include/message_logger.h"
 
+// Enum that describes allowed states for Parser
 typedef enum {
     COMMANDLINE,
     HEADERLINE,
     DATA
 } ParserState;
 
+// Typedef to abstract Headers hash
+typedef QHash<QString,QList<QString>> Headers;
+
+// HTTPParser class that implements parser for HTTP requests
 class HTTPParser {
     private:
+        // Private variables
         QString method;
         QString url;
         QString version;
         QString data;
-        QHash<QString, QList<QString>> headers;
+        Headers headers;
+
+        // Parser state
         ParserState state;
+
+        // Message logger
         MessageLogger logger;
+
+        // Private aux methods for parsing
         bool parseCommandLine(QString);
         bool parseHeaderLine(QString);
         bool parse(QString);
     public:
+        // Constructor
         HTTPParser();
+
+        // Getters
         QString getMethod();
         QString getURL();
         QString getHTTPVersion();
         QString getData();
+        Headers getHeaders();
+
+        // Parser
         bool parseRequest(QString);
+
+        // PrettyPrinter method
         void prettyPrinter();
-        QHash<QString,QList<QString>> getHeaders();
+
+        // Converts parsed HTTP to QString
+        QString toQString();
 };
 
 #endif // HTTPPARSER_H
