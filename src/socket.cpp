86edaf351d4743348f1d457276b9a87c2b159cb1
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-07-01 19:48:25 -0300

    Socket module - Doxygen documentation * Documented the header file of the socket module. * Documented the source file of the socket module.

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,9 +1,33 @@
 // Socket module - Source code.
 
+/**
+ * @file socket.cpp
+ * @brief Socket module - Source code.
+ *
+ * The socket module contains function wrappers and small abstractions of
+ * socket related functionalities. This source file contains the function
+ * implementations for this module.
+ *
+ */
+
 // Includes:
 #include "include/socket.h"
 
 // Function implementations:
+
+/**
+ * @fn int close_socket(int fd)
+ * @brief Function to close the connections of a socket.
+ * @param fd File descriptor for the socket whose connections will be closed.
+ * @return Returns 0 when successfully executed.
+ *
+ * Closes the connections of a socket by calling the shutdown function from
+ * 'sys/socket.h' with the socket file descriptor and a HOW argument of 0.
+ * This causes the socket to only stop receiving data. We them close the file
+ * descriptor of the socket with the close function.
+ *
+ */
+
 int close_socket(int fd) {
 
   // Shutdown the socket connections.
@@ -20,9 +44,38 @@ int close_socket(int fd) {
 
 }
 
+/**
+ * @fn int connect_socket (int fd, struct sockaddr *addr, socklen_t len)
+ * @brief Wrapper function for the connect 'sys/socket.h' function.
+ * @param fd File descriptor for the socket who will start the connection.
+ * @param addr Address of the socket who receives the connection.
+ * @param len Length of the socket who receives the connection.
+ * @return Returns 0 when the successfully executed and -1 if an error occurs.
+ *
+ * A simple wrapper function for the connect function in 'sys/socket.h'. This
+ * function doesn't implement anything new, but is necessary because a function
+ * named 'connect' already exists in the 'QObject' module, overwriting the
+ * connect function definition from 'sys/socket.h'.
+ *
+ */
+
 int connect_socket (int fd, struct sockaddr *addr, socklen_t len) {
   return connect(fd, addr, len);
 }
+
+/**
+ * @fn ssize_t read_socket(int fd, char *buffer, size_t size)
+ * @brief Wrapper function for the read function in 'unistd.h'.
+ * @param fd File descriptor for the socket where we read data from.
+ * @param buffer Location to store the data read.
+ * @param size Number of bytes to be read from the socket.
+ * @return Returns the number of bytes read on success and -1 if an error
+ * occurs.
+ *
+ * A simple wrapper function for the read function in 'unistd.h'. This
+ * function automatically adds a '\0' to the end of the information read.
+ *
+ */
 
 ssize_t read_socket(int fd, char *buffer, size_t size) {
 
