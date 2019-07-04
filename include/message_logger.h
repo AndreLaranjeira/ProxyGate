9166542d1f95d83c28a62b3b3437e67745e08760
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-07-03 23:09:48 -0300

    Message logger - Documentation and refactoring * Documented the MessageLogger class and it's files. * Removed unused methods and variables from the MessageLogger class header.

--- include/message_logger.h
+++ include/message_logger.h
@@ -1,5 +1,16 @@
 // Message logger module - Header file.
 
+/**
+ * @file message_logger.h
+ * @brief Message logger module - Header file.
+ *
+ * The message logger module contains the implementation of a simple message
+ * logger class that can be reused by any component on the project. This header
+ * file contains a header guard, library includes and the class headers for
+ * this module
+ *
+ */
+
 // Header guard:
 #ifndef MESSAGE_LOGGER_H
 #define MESSAGE_LOGGER_H
@@ -13,6 +24,57 @@
 using namespace std;
 
 // Class headers:
+
+/**
+ * @class MessageLogger
+ * @brief A simple message logger class used by other classes.
+ *
+ * This class implements a simple message logger that was made to give other
+ * classes a way to log messages in a window of the application.
+ *
+ * To use the MessageLogger in your class, you must ensure your class inherits
+ * from the QObject class and configure the MessageLogger.
+ *
+ * First, create the MessageLogger (it is preferable for it to be private)
+ * and a signal that transmits a QString in your class header:
+ *
+ *     class YourClass : public QObject {
+ *         Q_OBJECT
+ *
+ *         // Add these to your class:
+ *
+ *         signals:
+ *             void logMessage(QString);
+ *
+ *         private:
+ *             MessageLogger my_logger;
+ *
+ *         // Obviously, your class should have other methods and variables
+ *         // beside these (like a constructor)!
+ *
+ *     }
+ *
+ * Then, add the MessageLogger initialization to your class constructor:
+ *
+ *     YourClass::YourClass(parameters) : my_logger("Context name") {
+ *
+ *         // Connect the message logger signal to your signal:
+ *         connect(&my_logger, SIGNAL (sendMessage(QString)), this,
+                   SIGNAL (logMessage(QString)));
+ *
+ *         // The rest of your constructor code goes here!
+ *
+ *     }
+ *
+ * And you are all set! Now all messages generated using the message logger
+ * will emit the signal logMessage(QString), which should be treated by the
+ * MainWindow class or some other qualified class. In case you do NOT want
+ * YourClass to just propagate a signal, feel free to remove the logMessage
+ * signal from the class header and to treat the sendMessage signal from
+ * my_logger in whatever way you want to!
+ *
+ */
+
 class MessageLogger : public QObject {
 
   Q_OBJECT
@@ -29,21 +91,12 @@ class MessageLogger : public QObject {
     void warning(string);
 
     signals:
-    void sendMessage(QString);
+    void sendMessage(QString);  /**< Signal to send a log message. */
 
   private:
     // Variables:
-    string context;
-    bool connected = false;
-
-    // Methods:
-    void connectMainWindow();
+    string context;   /**< Context where the logger was created. */
 
 };
 
 #endif // MESSAGE_LOGGER_H
-
-/* TODO list:
- *  - Adapt this module to a Qt module with Qstring and a main window output.
- *
- */
--- src/message_logger.cpp
+++ src/message_logger.cpp
@@ -1,33 +1,109 @@
-// Error logger module - Source code.
+// Message logger module - Source code.
+
+/**
+ * @file message_logger.cpp
+ * @brief Message logger module - Source code.
+ *
+ * The message logger module contains the implementation of a simple message
+ * logger class that can be reused by any component on the project. This source
+ * file contains the class method implementations for this module.
+ *
+ */
 
 // Includes:
 #include "include/message_logger.h"
-#include "include/mainwindow.h"
 
 // Class methods:
+
+/**
+ * @fn MessageLogger::MessageLogger(std::string p_context)
+ * @brief Class constructor for the MessageLogger class.
+ * @param p_context Context private variable for the MessageLogger.
+ *
+ * This constructor creates a new instance of the MessageLogger class. Each
+ * instance has a p_context argument that configures the context private
+ * variable of the class, used to identify the source of a MessageLogger
+ * message.
+ *
+ */
+
 MessageLogger::MessageLogger(std::string p_context) {
   context = p_context;
 }
+
+/**
+ * @fn MessageLogger::~MessageLogger()
+ * @brief Class destructor for the MessageLogger class.
+ *
+ * This destructor destroys an instance of the MessageLogger class. It is
+ * currently empty!
+ *
+ */
 
 MessageLogger::~MessageLogger() {
 
 }
 
 // Public methods:
+
+/**
+ * @fn void MessageLogger::error(string message)
+ * @brief Method to log an error message.
+ * @param message Error message to be sent by the logger.
+ *
+ * This method formats an error message using the MessageLogger context, the
+ * 'Error' tag and the message argument. This error message is then emitted in
+ * a sendMessage signal and outputed in cerr.
+ *
+ */
+
 void MessageLogger::error(string message) {
   cerr << context << ": " << "[Error] " << message << endl;
   emit sendMessage(QString::fromStdString(context + ": " + "[Error] " + message));
 }
+
+/**
+ * @fn void MessageLogger::info(string message)
+ * @brief Method to log an information message.
+ * @param message Message to be sent by the logger.
+ *
+ * This method formats an information message using the MessageLogger context,
+ * the 'Info' tag and the message argument. This information message is then
+ * emitted in a sendMessage signal and outputed in cout.
+ *
+ */
 
 void MessageLogger::info(string message) {
   cout << context << ": " << "[Info] " << message << endl;
   emit sendMessage(QString::fromStdString(context + ": " + "[Info] " + message));
 }
 
+/**
+ * @fn void MessageLogger::success(string message)
+ * @brief Method to log a success message.
+ * @param message Message to be sent by the logger.
+ *
+ * This method formats a success message using the MessageLogger context, the
+ * 'Success' tag and the message argument. This success message is then emitted
+ * in a sendMessage signal and outputed in cout.
+ *
+ */
+
 void MessageLogger::success(string message) {
   cout << context << ": " << "[Success] " << message << endl;
   emit sendMessage(QString::fromStdString(context + ": " + "[Success] " + message));
 }
+
+/**
+ * @fn void MessageLogger::warning(string message)
+ * @brief Method to log a warning message.
+ * @param message Message to be sent by the logger.
+ *
+ * This method formats a warning message using the MessageLogger context, the
+ * 'Warning' tag and the message argument. This warning message is then emitted
+ * in a sendMessage signal and outputed in cout.
+ *
+ */
 
 void MessageLogger::warning(string message) {
   cout << context << ": " << "[Warning] " << message << endl;
