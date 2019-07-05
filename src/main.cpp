aa42c0c188bf8b8f913010d00f992d171e0e9ee9
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-07-04 22:41:04 -0300

    Main function and main window - Documentation * Documented main function. * Partially documented the main window module.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 // ProxyGate - Main function.
 
+/**
+ * @file main.cpp
+ * @brief Main file.
+ *
+ * This main file is a simple default Qt main file that starts the Qt
+ * application.
+ *
+ */
+
 // Qt includes:
 #include <QApplication>
 
@@ -7,6 +16,19 @@
 #include "include/mainwindow.h"
 
 // Main function:
+
+/**
+ * @fn int main(int argc, char *argv[])
+ * @brief Main function.
+ * @param argc Number of arguments.
+ * @param argv Program arguments.
+ * @return Program return code.
+ *
+ * This main function is a simple default Qt main function that starts the Qt
+ * application.
+ *
+ */
+
 int main(int argc, char *argv[]) {
 
   // Class declarations:
